735cad30bee6bc645bd7ca365ae94743cad224ec
Diego Aaron Figueroa Campos <16460315@colima.tecnm.mx>
2020-10-14 19:59:48 -0500

    Add animal collage image views
    Add styles, views and images.
    
    Co-authored-by: spa_brayan@hotmail.com <spa_brayan@hotmail.com>

---
+++ extinction_animals.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>IAkinator</title>
+	<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" type="text/css" href="css/extinction_animals.css">
+</head>
+
+<body>
+    <main class="title-wrapper">
+        <h2>Animales en peligro de extinción en México</h2>
+        <div class="buttons-container">
+            <a href="index.php"><button>Iniciar juego</button></a>
+        </div>
+    </main>
+	<main class="animal-wrapper">
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2 class="animal-name">Ajolote</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Sardinilla Yucateca</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Pez con flecos del Golfo de México</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Chara enana</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Iguana espinosa del Golfo</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Liebre de Tehuantepec</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Vaquita marina</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Víbora pigmea mexicana</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Perrito de la pradera mexicano</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Teporingo</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Mapache de Cozumel</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Escorpión mexicano</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Lagarto topo mexicano</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Charal de Alchichica</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Mochuelo tamaulipeco</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Chingolo serrano</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Nauyaca guerrerense de montaña</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Tortuga de caja acuática</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Murciélago platanero</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Mofeta moteada pigmea</h2>
+            </article>
+        </section>
+	</main>
+</body>
+
+</html>
---
+++ start.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>IAkinator</title>
+	<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" type="text/css" href="css/start.css">
+</head>
+
+<body>
+    <main class="title-wrapper">
+        <div id="show-it">
+            <h2>Animales endémicos de México</h2>
+            <div class="buttons-container">
+                <a href="extinction_animals.php"><button>Ver más animales</button></a>
+                <a href="index.php"><button>Iniciar juego</button></a>
+            </div>
+        </div>
+        <div id="hide-it">
+            <h2>Elije un animal en tu mente y recuérdalo durante todo el juego</h2>
+        </div>
+    </main>
+	<main class="animal-wrapper">
+        <section class="animal-card ajolote">
+            <article class="animal-card__name">
+                <h2 class="animal-name">Ajolote</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Sardinilla Yucateca</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Pez con flecos del Golfo de México</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Chara enana</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Iguana espinosa del Golfo</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Liebre de Tehuantepec</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Vaquita marina</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Víbora pigmea mexicana</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Perrito de la pradera mexicano</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Teporingo</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Mapache de Cozumel</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Escorpión mexicano</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Lagarto topo mexicano</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Charal de Alchichica</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Mochuelo tamaulipeco</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Chingolo serrano</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Nauyaca guerrerense de montaña</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Tortuga de caja acuática</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Murciélago platanero</h2>
+            </article>
+        </section>
+        <section class="animal-card">
+            <article class="animal-card__name">
+                <h2>Mofeta moteada pigmea</h2>
+            </article>
+        </section>
+	</main>
+</body>
+
+</html>
